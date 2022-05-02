0bd01a263145df8be8158ee70d7d4904c13c559a
gabrielle-esguerra <103225491+gabrielle-esguerra@users.noreply.github.com>
2022-05-03 09:27:32 +1200

    Adding a new album

--- album_rate.py
+++ album_rate.py
@@ -1,37 +1,59 @@
-## album_rate.py
+## album_rater.py
 # stores title, artist, genre and rating of an music albums
 # allows the user to add, delete and edit an album
 # allows user to rate album and recommends album of same genre
 
 def printing(dictionary):
-    for id, song in dictionary.items():
+    for id, album in dictionary.items():
         """
         Accepts a dictionary , loops through it and
         prints out the album's information
         """
         print("Id: {}\tTitle: {}\tArtist: {}\tGenre: {}\tRating: {}".format (id,
-                                                                        song["title"],
-                                                                        song["artist"],
-                                                                        song["genre"],
-                                                                        song["rating"],))
+                                                                        album["title"],
+                                                                        album["artist"],
+                                                                        album["genre"],
+                                                                        album["rating"],))
 
 def add(dictionary):
-dictionary.update({num{"title": , "artist": , "genre}})
-    
-#def edit():
+    num = len(dictionary) + 1
+    dictionary[num] = {}
+    dictionary[num]["title"] = input("Album title: ")
+    dictionary[num]["artist"] = input("Artist: ")
+    dictionary[num]["genre"] = input("Genre: ")
+    dictionary[num]["rating"] = input("Rating: ")
+    return dictionary
 
 def delete():
     del dictionary[""]
     return dictionary
 
+#def edit():
+
 #def rate():
 
 #def recommend():
 
-
-albums = {}
 if __name__ == "__main__":
-    albums = {1:{"title":"Blond", "artist":"Frank Ocean", "genre":"Contemporary R&B", "rating":98},
-              2:{"title":"Call Me If You Get Lost", "artist":"Tyler the Creator", "genre":"Hip-Hop / Rap", "rating":97},
-              3:{"title":"Ctrl", "artist":"SZA", "genre":"R&B / Soul", "rating":98}}
-    printing(albums)
+    albums = {1: {"title":"Blond", "artist":"Frank Ocean", "genre":"Contemporary R&B", "rating":98},
+              2: {"title":"Call Me If You Get Lost", "artist":"Tyler the Creator", "genre":"Hip-Hop / Rap", "rating":97},
+              3: {"title":"Ctrl", "artist":"SZA", "genre":"R&B / Soul", "rating":98}}
+    mode = int(input("\nMenu \n"
+                     "1. Print out all albums \n"
+                     "2. Add an album \n"
+                     "3. Delete an album \n"
+                     "4. Edit an album\n"
+                     "5. Rate an album \n"
+                         "6. Exit \n"))
+    if mode == 1:
+        printing(albums)
+    elif mode == 2:
+        add(albums)
+    elif mode == 3:
+        delete(albums)
+    elif mode == 4:
+        edit(albums)
+    elif mode == 5:
+        rate(albums)
+    #elif mode == 6:
+       
